3eec4d074e68a43b8a3191a62af06f05be6cd10d
David Arnold <dar@xoe.solutions>
2020-10-12 16:42:45 -0500

    Add ECDSA parsing

--- src/lib.rs
+++ src/lib.rs
@@ -266,7 +266,7 @@ mod keylog;
 pub mod internal {
     /// Functions for parsing PEM files containing certificates/keys.
     pub mod pemfile {
-        pub use pemfile::{certs, rsa_private_keys, pkcs8_private_keys};
+        pub use pemfile::{certs, rsa_private_keys, ec_private_keys, pkcs8_private_keys};
     }
 
     /// Low-level TLS message parsing and encoding functions.
